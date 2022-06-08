3c59a54ffb63e2770a0bd312f8347a491ea356c5
Syomuch <andrijsimko@gmail.com>
2022-06-08 15:39:55 +0300

    $ git config --global user.name "Syomuch" $ git config --global user.email andrijsimko@gmail.com

---
+++ src/main/java/com/course/site/SiteApplication.java
@@ -0,0 +1,13 @@
+package com.course.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan(value = "controllers")
+public class SiteApplication {
+
+	public static void main(String[] args) {SpringApplication.run(SiteApplication.class, args);
+	}
+
+}
